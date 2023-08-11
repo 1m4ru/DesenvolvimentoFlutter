6246adb07a5bebe5f21685c1f8cdc9c0d7320397
MARUAN IBRAHIM MOUSSA <maruan.moussa@alunos.sc.senac.br>
2023-08-11 20:41:44 -0300

    novas funcionalidades aprendidas

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp( //widget
+  runApp(MaterialApp(
+    //widget
     debugShowCheckedModeBanner: false,
-      home: HomePage(), // atributo
-
-  )
-  
-  );  
+    home: HomePage(), // atributo
+    theme: ThemeData(primarySwatch: Colors.purple),
+  ));
 }
 
 // Tem somente dois tipo de widget de estado, um chamado STATELESS e STATEFULL
@@ -16,6 +14,7 @@ void main() {
 //STATEFULL é como se fosse uma pagina dinamica
 PageController pageController = PageController();
 int paginaAtual = 0;
+int contador = 0;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -28,72 +27,130 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Nubank'),
-        backgroundColor: Colors.purple,
-        elevation: 30,
-        // centerTitle: true,
-      ),
+        appBar: AppBar(
+          title: Text('Nubank'),
+          backgroundColor: Colors.purple,
+          elevation: 30,
+          // centerTitle: true,
+        ),
 
-      // body: Column(
-      //   mainAxisAlignment: MainAxisAlignment.start,
-      //   children: [
-      //     Container(
-      //   // margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      //   // padding: EdgeInsets.all(20),
-      //   width: double.infinity,
-      //   height: 200,
-      //   decoration: BoxDecoration(
-      //     color: Color.fromARGB(225, 197, 51, 255),
-      //     borderRadius: BorderRadius.circular(20),
-      //     gradient: LinearGradient(colors: [Colors.red, Colors.blue],)
-      //   ),
-      //   child: Text('Container 1'),
-      // ),
-      
-      // Row(
-      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //   children: [
-      //     Container(
-      //       width: 50,
-      //       height: 50,
-      //       color: Colors.red,
-      //     ),
-      //     Container(
-      //       width: 50,
-      //       height: 50,
-      //       color: Colors.red,
-      //     ),
-      //   ],
-      // )
+        // body: Column(
+        //   mainAxisAlignment: MainAxisAlignment.start,
+        //   children: [
+        //     Container(
+        //   // margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        //   // padding: EdgeInsets.all(20),
+        //   width: double.infinity,
+        //   height: 200,
+        //   decoration: BoxDecoration(
+        //     color: Color.fromARGB(225, 197, 51, 255),
+        //     borderRadius: BorderRadius.circular(20),
+        //     gradient: LinearGradient(colors: [Colors.red, Colors.blue],)
+        //   ),
+        //   child: Text('Container 1'),
+        // ),
 
-      //   ],
+        // Row(
+        //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //   children: [
+        //     Container(
+        //       width: 50,
+        //       height: 50,
+        //       color: Colors.red,
+        //     ),
+        //     Container(
+        //       width: 50,
+        //       height: 50,
+        //       color: Colors.red,
+        //     ),
+        //   ],
+        // )
+
+        //   ],
+
+        //   ),
+        drawer: Drawer(
         
-      //   ),
-    body: PageView(
-      controller: pageController ,
-      children: [
-        Container(color: Colors.green,),
-        Container(color: Colors.blue,),
-        Container(color: Colors.redAccent,),
-      ],
-    ),
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text('Maruzin'), 
+                accountEmail: Text('maruanibrahim22@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  child: Text('M'),
+                ),
+                ),
+                ListTile(
+                  title: Text('Minha Conta'),
+                  subtitle: Text('Dados da sua conta'),
+                  trailing: Icon(Icons.money),
+                  leading: Icon(Icons.money_off),
+                  onTap: (){},
+                )
+            ],
+          ),
+        ),
+        body: PageView(
+          controller: pageController,
+          children: [
+            Center(
+              child: Container(
+                child: Text(contador.toString(), style: TextStyle(fontSize: 66 )),
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+            ),
+            Container(
+              color: Colors.redAccent,
+            ),
+          ],
+        ),
         bottomNavigationBar: BottomNavigationBar(
-          currentIndex: paginaAtual,
-          selectedItemColor: Colors.red,
-          onTap: (page){
-         pageController.animateToPage(page, duration: Duration(microseconds: 200), curve: Curves.ease);
+            currentIndex: paginaAtual,
+            selectedItemColor: Colors.red,
+            onTap: (page) {
+              pageController.animateToPage(page,
+                  duration: Duration(microseconds: 200), curve: Curves.ease);
 
-        setState(() {
-           paginaAtual = page;
-        });
-        
-         print(paginaAtual);
-          } ,items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorites'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-        ]),
-    );
+              setState(() {
+                paginaAtual = page;
+              });
+
+              print(paginaAtual);
+            },
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.favorite), label: 'Favorites'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.search), label: 'Search'),
+            ]),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                   if(contador > 0){
+                  contador--;
+                }
+                  
+                });
+              },
+              child: Icon(Icons.remove),
+            ),
+            SizedBox(width: 14,),
+            FloatingActionButton(
+              onPressed: () {
+               setState(() {
+                  contador++;
+                  
+               });
+              },
+              child: Icon(Icons.add),
+            )
+          ],
+        ));
   }
 }
